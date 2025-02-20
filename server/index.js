810341897d848d47e3b820a5152001088c2e34c7
lietuviskalba <mantasling@gmail.com>
2025-02-20 12:57:52 +0200

    added cors

--- server/index.js
+++ server/index.js
@@ -1,13 +1,22 @@
 // server/index.js
 const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
 const { v4: uuidv4 } = require("uuid");
-const pool = require("./db"); // Import the PostgreSQL pool
+const pool = require("./db");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(helmet());
 
+app.use(
+  cors({
+    origin: "https://lingaitis.com/media_ranker",
+    optionsSuccessStatus: 200,
+  })
+);
+app.use(express.json());
 // GET /api/media_records - Return all media_records
 app.get("/api/media_records", async (req, res) => {
   try {
