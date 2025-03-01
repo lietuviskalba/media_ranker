357a20f2c9ff3a2d2c844bdb78567f7cd49dc03a
lietuviskalba <mantasling@gmail.com>
2025-03-02 00:49:23 +0200

    reset db con prod and local

--- server/db.js
+++ server/db.js
@@ -1,16 +1,19 @@
-// server/db.js
-require("dotenv").config(); // Load environment variables from .env
+// db.js
+require("dotenv").config();
 
 const { Pool } = require("pg");
 
+// Determine whether we're running locally or in production
+const isProduction = process.env.NODE_ENV === "production";
+
+// Use a local connection string when not in production
+const connectionString = isProduction
+  ? process.env.DATABASE_URL
+  : process.env.LOCAL_DATABASE_URL;
+
 const pool = new Pool({
-  // user: process.env.PGUSER,
-  // host: process.env.PGHOST,
-  // database: process.env.PGDATABASE,
-  // password: process.env.PGPASSWORD,
-  // port: process.env.PGPORT,
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  connectionString,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = pool;
