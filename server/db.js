c84286fb11fc53f312f71fc89892bcc4e36db501
lietuviskalba <mantasling@gmail.com>
2025-03-01 22:53:55 +0200

    FIX db connection

--- server/db.js
+++ server/db.js
@@ -4,11 +4,12 @@ require("dotenv").config(); // Load environment variables from .env
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  // user: process.env.PGUSER,
+  // host: process.env.PGHOST,
+  // database: process.env.PGDATABASE,
+  // password: process.env.PGPASSWORD,
+  // port: process.env.PGPORT,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
 
