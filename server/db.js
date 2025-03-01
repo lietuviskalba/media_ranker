db2c41a9847cab7695dd398813cbb192bdbfbc99
lietuviskalba <mantasling@gmail.com>
2025-03-01 23:51:38 +0200

    TEMP con test

--- server/db.js
+++ server/db.js
@@ -4,12 +4,7 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 // Determine whether we're running locally or in production
-const isProduction = process.env.NODE_ENV === "production";
-
-// Use a local connection string when not in production
-const connectionString = isProduction
-  ? process.env.DATABASE_URL
-  : process.env.LOCAL_DATABASE_URL;
+const connectionString = process.env.DATABASE_URL;
 
 const pool = new Pool({
   connectionString,
