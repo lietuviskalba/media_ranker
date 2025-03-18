08392b25c1fd97c58b52e043ab669b42656abb0a
lietuviskalba <mantasling@gmail.com>
2025-03-18 11:44:00 +0200

    server db update

--- server/db.js
+++ server/db.js
@@ -1,20 +1,14 @@
 // db.js
 require("dotenv").config();
-
 const { Pool } = require("pg");
 
-// Determine whether we're running locally or in production
+// Read environment variables
 const isProduction = process.env.NODE_ENV === "production";
-
-// Use a local connection string when not in production
-const connectionString = isProduction
-  ? process.env.DATABASE_URL
-  : process.env.LOCAL_DATABASE_URL;
+const connectionString = process.env.DATABASE_URL;
 
 const pool = new Pool({
   connectionString,
-  host: "127.0.0.1",
-  ssl: isProduction ? { rejectUnauthorized: false } : false,
+  ssl: isProduction ? false : false, // No need for SSL on the local server
 });
 
 pool
