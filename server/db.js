0d501f29acb411ccb94b0a124e85062a6e7caff2
lietuviskalba <mantasling@gmail.com>
2025-03-18 20:38:11 +0200

    db no local change

--- server/db.js
+++ server/db.js
@@ -1,19 +1,16 @@
-// db.js
+// server/db.js
 require("dotenv").config();
-
 const { Pool } = require("pg");
 
-// Determine whether we're running locally or in production
 const isProduction = process.env.NODE_ENV === "production";
+const connectionString = process.env.DATABASE_URL;
 
-// Use a local connection string when not in production
-const connectionString = isProduction
-  ? process.env.DATABASE_URL
-  : process.env.LOCAL_DATABASE_URL;
+if (!connectionString) {
+  console.error("DATABASE_URL is not set!");
+}
 
 const pool = new Pool({
   connectionString,
-  host: "127.0.0.1",
   ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
